fd37c45b69e031996bdcd68beaa5ebd74b96b1c2
golima <lima01@gmail.com>
2021-06-03 23:14:39 -0400

    Fix a syntax error

--- src/api/actions.js
+++ src/api/actions.js
@@ -2,23 +2,24 @@ import { API_HOST } from '../constants';
 
 function getActions(lastActionId, callback) {
   return fetch(`${API_HOST}/actions?orderBy=time&limit=5${lastActionId ? `&offset=${lastActionId}` : ''}`)
-  .then(res => res.json())
-  .then((result) => {
-    callback(result?.actions);
-  }, (error) => {
-    callback(error, true);
-  })
+    .then(res => res.json())
+    .then((result) => {
+      callback(result?.actions);
+    }, (error) => {
+      callback(error, true);
+    })
 }
 
 function getAction(actionId, callback) {
   if (!actionId) return;
   return fetch(`${API_HOST}/actions/${actionId}`)
-  .then((res) => res.json())
-  .then((result) => {
-    callback(result);
-  }), (error) => {
-    callback(error, true);
-  }
+    .then((res) => res.json())
+    .then((result) => {
+      callback(result);
+    }, 
+    (error) => {
+      callback(error, true);
+    })
 }
 
 function voteAction(actionId, callback) {
@@ -26,12 +27,12 @@ function voteAction(actionId, callback) {
   return fetch(`${API_HOST}/actions/${actionId}/vote`, {
     method: 'post',
   })
-  .then(res => res.json())
-  .then((result) => {
-    callback(actionId, result.vote_count);
-  }, () => {
-    callback(actionId, undefined, true);
-  });
+    .then(res => res.json())
+    .then((result) => {
+      callback(actionId, result.vote_count);
+    }, () => {
+      callback(actionId, undefined, true);
+    });
 }
 
 export { getActions, getAction, voteAction };
